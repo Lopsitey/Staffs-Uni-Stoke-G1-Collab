e2071cfd5456f2c75c996136642ac45a9e290128
Ethan Bashforth George <ethang2705@gmail.com>
2026-03-23 09:57:09 +0000

    reset

--- Source/G1Collab/G1Collab.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "G1Collab.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, G1Collab, "G1Collab" );
